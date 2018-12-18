fecdb538cd198fb56a4cf9ded94ed17dea2b7c15
Vince Bloise <vbloise3@verizon.net>
2018-12-17 21:57:14 -0500

    added another api to data.service and changed home html

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,6 +9,10 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getUsers() {
+    return this.http.get('https://reqres.in/api/users?page=2');
+  }
+
+  getUsersWithParams() {
     // Initialize Params Object
     let params = new HttpParams();
 
